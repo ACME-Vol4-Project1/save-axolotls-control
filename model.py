09526caf67b698654740b3bd4afb6edc20d549a1
Emeline Thygerson <emelinehope2002@gmail.com>
2026-03-26 21:53:04 +0000

    updated ODE, added carrying capacity and modified treatment options

--- model.py
+++ model.py
@@ -3,29 +3,29 @@ from matplotlib import pyplot as plt
 from scipy.integrate import solve_ivp
 
 # define the model
-def model_equation(t, y, params=[.09, .005, .001, .004, .001, .005, .001]):
+def model_equation(t, y, control1, control2, alpha, gamma, zeta, beta, delta, mu):
     """ODE equation for our Chytrid fungus model. The y contains the values of
     each compartment at time t, where the compartments are
         - S: susceptible
         - I: infected
         - T1: treatment 1 (frog sauna)
         - T2: treatment 2 (antifungal bath)
-        - R1: deceased but can still transmit the fungus
-        - R2: host and fungus deceased
+        - R: deceased but can still transmit the fungus
+
+        Parameters:
+        - control1: control parameters u1 and u2 (functions of t)
+        - control2: functions of u1 and u2
+        - other parameters: express movement between components
     """
-    S, I, T1, T2, R1, R2 = y
-    alpha, gamma, eta, zeta, beta, delta, xi = params
+    S, I, T1, T2, R = y
+    u1, u2 = control1
+    K, eta, xi = control2
 
-    # # introduce periodic seasonal transmission   (not doing this for now, might add back in later)
-    # sigma = 6    # amplitude of seasonal variation in transmission
-    # alpha = alpha1*(1 + sigma*np.cos(2*np.pi*t))
-    
     # define transition equations
-    dS = -alpha*I*S - gamma*I*R1 + eta*T2 - zeta*S*(sum(y) - (T1 + T2 + R1 + R2))
-    dI = alpha*I*S + gamma*I*R1 - zeta*I*(sum(y) - (T1 + T2 + R1 + R2)) - xi*I - beta*I
-    dT1 = zeta*(I + S)*(sum(y) - (T1 + T2 + R1 + R2)) 
-    dT2 = xi*I - eta*T2
-    dR1 = beta*I - delta*R1                            
-    dR2 = delta*R1                                      
+    dS = -alpha*I*S + gamma*T1 + eta(u2)*T2 - zeta*S*(1 - (S + I)/K(u1))
+    dI = alpha*I*S - zeta*I*(1 - (S + I)/K(u1)) - xi(u2)*I - beta*I
+    dT1 = zeta*(I + S)*(1 - (S + I)/K(u1)) - gamma*T1
+    dT2 = xi(u2)*I - eta(u2)*T2 - (1 - eta(u2))*T2
+    dR = beta*I + (1 - eta(u2))*T2
 
-    return [dS, dI, dT1, dT2, dR1, dR2]
+    return [dS, dI, dT1, dT2, dR]
